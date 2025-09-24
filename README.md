98ec789691aad1a3b417ddf7e7dc6c29de71b3a0
paolomorettin <morettin.paolo@gmail.com>
2025-09-24 10:15:00 +0200

    added MIT license badge + reformatting

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # WM*PY
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 [![Build and Test (Python 3.9+, macOS, Linux)](https://github.com/unitn-sml/wmpy/actions/workflows/build-and-test.yml/badge.svg)](https://github.com/unitn-sml/wmpy/actions/workflows/build-and-test.yml)
 [![codecov](https://codecov.io/github/unitn-sml/wmpy/branch/master/graph/badge.svg?token=VIN9CAWNZP)](https://codecov.io/github/unitn-sml/wmpy)
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,8 +69,8 @@ exclude_patterns = [""]
 
 # Readthedocs theme
 # on_rtd is whether on readthedocs.org, this line of code grabbed from docs.readthedocs.org...
-#on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-#if not on_rtd:  # only import and set the theme if we're building docs locally
+# on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+# if not on_rtd:  # only import and set the theme if we're building docs locally
 #    import sphinx_rtd_theme
 
 #    html_theme = "sphinx_rtd_theme"
