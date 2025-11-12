77cc1064baa20b9d2e6f5b4c7e6b8af18ac32a7f
paolomorettin <morettin.paolo@gmail.com>
2025-11-12 15:23:39 +0100

    minor fix changing import order

--- src/wmpy/core/__init__.py
+++ src/wmpy/core/__init__.py
@@ -7,6 +7,6 @@ It exposes:
 - Polytope: the internal class for handling convex polytopes
 """
 
-from .assignmentconverter import AssignmentConverter
 from .polynomial import Polynomial
 from .polytope import Polytope
+from .assignmentconverter import AssignmentConverter
